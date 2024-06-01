6baef0020d9629925ef8f18f6b6b1d428085efe0
陳以婕 <153205422+11236017@users.noreply.github.com>
2024-06-01 19:45:18 +0800

    Delete project113209 directory

--- project113209/app113209/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'phone', 'department_id', 'position_id', 'branch_id', 'module', 'gender', 'is_approved')
-    list_filter = ('department_id', 'position_id', 'branch_id', 'gender', 'is_approved')
-
-admin.site.register(User, UserAdmin)
--- project113209/app113209/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class App113209Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app113209'
